6a8ceb83c70b646adf9e6a74137024544352ae66
Andres Collazos <andrescollazosc@gmail.com>
2024-09-11 16:09:25 -0500

    refactor(portfolio): refactor SocialMedia model
    Refs: TASK-05-ACC

--- src/IdeaCompany.Portfolio.Core/Portfolios/Models/SocialMedia.cs
+++ src/IdeaCompany.Portfolio.Core/Portfolios/Models/SocialMedia.cs
@@ -2,11 +2,7 @@ namespace IdeaCompany.Portfolio.Core.Portfolios.Models;
 
 public class SocialMedia
 {
-    public Guid Id { get; set; }
-    public Guid PortfolioId { get; set; }
     public string Url { get; set; } = string.Empty;
     public string Icon { get; set; } = string.Empty;
     public string Tag { get; set; } = string.Empty;
-
-    public Portfolio Portfolio { get; set; }
 }
