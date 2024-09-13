d9129785108b4b45a007a7aeebb5c57f406ec8f7
Andres Collazos <andrescollazosc@gmail.com>
2024-09-13 12:58:25 -0500

    feat(portfolio): change email field by email list
     - add new EF migration
    
    Refs: TASK-05-ACC

---
+++ src/IdeaCompany.Portfolio.Data.Ef/Migrations/20240913175334_RemoveEmailFieldToPortfolio.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IdeaCompany.Portfolio.Data.Ef.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveEmailFieldToPortfolio : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Portfolios");
+
+            migrationBuilder.RenameColumn(
+                name: "ComanyName",
+                table: "WorkExperiences",
+                newName: "CompanyName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CompanyName",
+                table: "WorkExperiences",
+                newName: "ComanyName");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Portfolios",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
---
+++ src/IdeaCompany.Portfolio.Data.Ef/Migrations/20240913175649_AddEmailsFieldToPortfolio.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IdeaCompany.Portfolio.Data.Ef.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEmailsFieldToPortfolio : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Emails",
+                table: "Portfolios",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Emails",
+                table: "Portfolios");
+        }
+    }
+}
