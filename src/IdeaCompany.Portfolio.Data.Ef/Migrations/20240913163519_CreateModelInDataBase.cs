339f65dac83dfdddf766efd2e675d3bd27d6059e
Andres Collazos <andrescollazosc@gmail.com>
2024-09-13 11:43:49 -0500

    feat(portfolio): add first EF migration
    Refs: TASK-05-ACC

---
+++ src/IdeaCompany.Portfolio.Data.Ef/Migrations/20240913163519_CreateModelInDataBase.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IdeaCompany.Portfolio.Data.Ef.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateModelInDataBase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Portfolios",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PortfolioTag = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    OwnerFirstName = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: false),
+                    OwnerLastName = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    PhoneNumbers = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SocialMedias = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Summary = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Portfolios", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EmailSettings",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PortfolioId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    PasswordApplication = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    SmtpClient = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Port = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmailSettings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EmailSettings_Portfolios_PortfolioId",
+                        column: x => x.PortfolioId,
+                        principalTable: "Portfolios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WorkExperiences",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PortfolioId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: false),
+                    ComanyName = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: false),
+                    UrlCompany = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: false),
+                    IconCompany = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorkExperiences", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_WorkExperiences_Portfolios_PortfolioId",
+                        column: x => x.PortfolioId,
+                        principalTable: "Portfolios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmailSettings_PortfolioId",
+                table: "EmailSettings",
+                column: "PortfolioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Portfolios_PortfolioTag",
+                table: "Portfolios",
+                column: "PortfolioTag",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkExperiences_PortfolioId",
+                table: "WorkExperiences",
+                column: "PortfolioId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmailSettings");
+
+            migrationBuilder.DropTable(
+                name: "WorkExperiences");
+
+            migrationBuilder.DropTable(
+                name: "Portfolios");
+        }
+    }
+}
