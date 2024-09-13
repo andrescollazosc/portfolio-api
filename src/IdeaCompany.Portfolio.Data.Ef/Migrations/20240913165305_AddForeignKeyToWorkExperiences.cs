75caeab38a563896389dc050f985970d132fee84
Andres Collazos <andrescollazosc@gmail.com>
2024-09-13 11:55:12 -0500

    feat(portfolio): add FK to WorkExperienceMapping
     - add new EF migration
    
    Refs: TASK-01-ACC

---
+++ src/IdeaCompany.Portfolio.Data.Ef/Migrations/20240913165305_AddForeignKeyToWorkExperiences.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IdeaCompany.Portfolio.Data.Ef.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddForeignKeyToWorkExperiences : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "PortfolioId1",
+                table: "WorkExperiences",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkExperiences_PortfolioId1",
+                table: "WorkExperiences",
+                column: "PortfolioId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_WorkExperiences_Portfolios_PortfolioId1",
+                table: "WorkExperiences",
+                column: "PortfolioId1",
+                principalTable: "Portfolios",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_WorkExperiences_Portfolios_PortfolioId1",
+                table: "WorkExperiences");
+
+            migrationBuilder.DropIndex(
+                name: "IX_WorkExperiences_PortfolioId1",
+                table: "WorkExperiences");
+
+            migrationBuilder.DropColumn(
+                name: "PortfolioId1",
+                table: "WorkExperiences");
+        }
+    }
+}
